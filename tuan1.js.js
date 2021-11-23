0dfbd762411789469e0f27703cabf89ec74a1318
Thu Uyên <89678958+thuuyen310@users.noreply.github.com>
2021-11-23 14:29:20 +0700

    Bài tập tuần 1

---
+++ tuan1.js.js
@@ -0,0 +1,19 @@
+/*//Bài 1:
+var i = 0.5;
+var u = 0.5;
+var sum = i+u;
+console.log(sum);*/
+/*//Bài 2:
+var a = 310;
+var b = 22;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a*a+b*b);*/
+/*//Bài 3:
+function math(a, b){
+    return a+b;
+}
+console.log(math(10,8));*/
