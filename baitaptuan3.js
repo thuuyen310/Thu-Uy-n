fcf636b3f3f51ace1e5084d80ec458ca4fee7671
Thu Uyên <89678958+thuuyen310@users.noreply.github.com>
2021-12-13 09:40:39 +0700

    Bai tap tuan 3(thieu bai 6)

---
+++ baitaptuan3.js
@@ -0,0 +1,85 @@
+// //Ex1:
+// function draw(n){
+// for( let i=0; i<n; i++){
+//     let str = "";
+//     for( let j=0; j<n; j++){
+//         str += "*";
+//     }
+//     console.log(str)
+// }
+// }
+// draw(5)
+// //Ex2:
+// function shape (n,m){
+//     for(let i=0; i<n; i++){
+//         let result = "";
+//         for( let j=0; j<m; j++){
+//             result += "*";
+//         }
+//         console.log(result)
+//     }
+// }
+// shape(3,5)
+// //Ex3:
+// function tamgiac (n){
+//     let r="*";
+//     for( let i = 0;i<n;i++){
+//         let result="";
+//         for(let j=0;j<=i;j++){
+//             result += "*";
+//             r++;
+//         }
+//         console.log(result)
+//     }
+// }
+// tamgiac(3)
+// //Ex4:
+// function triple (n){
+//     for( let i=n; i>=0; i--){
+//         let result ="";
+//         for( let j=0; j<i;j++){
+//             result += "*";
+//         }
+//         console.log(result)
+//     }
+// }
+// triple(3)
+// //Ex5:
+// const n = 4;
+// for (let i = 0; i < n; i++){
+//     let result = "";
+//     for (let j = 0; j < n; j++){
+//         if(i == 0 || i == n-1 || j == 0 || j == n-1){
+//             result += "* ";
+//         } else{
+//             result += "- "
+//         }
+//     }
+//     console.log(result)
+// }
+//Ex6:
+
+// //Ex7:
+// function hcn(n,m){
+//     for( let i=1; i<=n;i++){
+//         let result ="";
+//         for(let j=0; j<m; j++){
+//             result += i;
+//         }
+//         console.log(result)
+//     }
+// }
+// hcn(2,4)
+// //Ex8:
+// function so (n){
+//     let r=1;
+//     for( let i=1; i<=n; i++){
+//         let result ="";
+//         for( let j =0; j<i; j++){
+//             result += r;
+//             r++;
+//         }
+//         console.log(result)
+//     }
+// }
+// so(3)
